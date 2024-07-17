f953b34018f0e7acd95048b783a576d79af02824
YunhoNam <nam100400@naver.com>
2024-07-17 20:51:11 +0900

    #14 - 게시글 관련 뷰 테스트 작성

---
+++ src/main/java/org/yunho/boardservice/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package org.yunho.boardservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
