a4d7ecab81c1d2ab65f6a053259f6755fea5d496
YunhoNam <nam100400@naver.com>
2024-07-17 19:42:58 +0900

    #1 - Data REST 설정

--- src/main/java/org/yunho/boardservice/repository/ArticleCommentRepository.java
+++ src/main/java/org/yunho/boardservice/repository/ArticleCommentRepository.java
@@ -1,7 +1,9 @@
 package org.yunho.boardservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.yunho.boardservice.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/org/yunho/boardservice/repository/ArticleRepository.java
+++ src/main/java/org/yunho/boardservice/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package org.yunho.boardservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.yunho.boardservice.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
