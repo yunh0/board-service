819769b05661f5e19331764a030f8d249004211a
YunhoNam <nam100400@naver.com>
2024-07-17 23:51:28 +0900

    #14 - 시큐리티 설정을 컨트롤러 테스트에 반영

--- src/test/java/org/yunho/boardservice/controller/ArticleControllerTest.java
+++ src/test/java/org/yunho/boardservice/controller/ArticleControllerTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.yunho.boardservice.config.SecurityConfig;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
