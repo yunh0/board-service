ce7fd80fa279416bcbb5a65c363d5bd943d270c4
YunhoNam <nam100400@naver.com>
2024-07-21 21:40:47 +0900

    #36 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용함

--- src/main/java/org/yunho/boardservice/repository/UserAccountRepository.java
+++ src/main/java/org/yunho/boardservice/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package org.yunho.boardservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.yunho.boardservice.domain.UserAccount;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
--- src/test/java/org/yunho/boardservice/controller/ArticleControllerTest.java
+++ src/test/java/org/yunho/boardservice/controller/ArticleControllerTest.java
@@ -219,7 +219,7 @@ class ArticleControllerTest {
     }
 
     private UserAccountDto createUserAccountDto() {
-        return UserAccountDto.of(1L,
+        return UserAccountDto.of(
                 "uno",
                 "pw",
                 "uno@mail.com",
