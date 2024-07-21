a1d1d0725ea3092d3385def3c9cc84894229d765
YunhoNam <nam100400@naver.com>
2024-07-21 22:48:23 +0900

    #42 - 댓글 저장, 삭제 기능 구현

---
+++ src/main/java/org/yunho/boardservice/controller/ArticleCommentController.java
@@ -0,0 +1,37 @@
+package org.yunho.boardservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.yunho.boardservice.dto.UserAccountDto;
+import org.yunho.boardservice.dto.request.ArticleCommentRequest;
+import org.yunho.boardservice.service.ArticleCommentService;
+
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+@Controller
+public class ArticleCommentController {
+
+    private final ArticleCommentService articleCommentService;
+
+    @PostMapping ("/new")
+    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
+        // TODO: 인증 정보를 넣어줘야 한다.
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
+                "uno", "pw", "uno@mail.com", null, null
+        )));
+
+
+        return "redirect:/articles/" + articleCommentRequest.articleId();
+    }
+
+    @PostMapping ("/{commentId}/delete")
+    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
+        articleCommentService.deleteArticleComment(commentId);
+
+        return "redirect:/articles/" + articleId;
+    }
+
+}
