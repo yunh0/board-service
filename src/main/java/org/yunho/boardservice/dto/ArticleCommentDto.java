6625ee6b4b46699ec7ccd034cf0d916169a53e54
YunhoNam <nam100400@naver.com>
2024-07-21 16:23:36 +0900

    #21 - 댓글 서비스 로직 테스트와 골격 잡기

---
+++ src/main/java/org/yunho/boardservice/dto/ArticleCommentDto.java
@@ -0,0 +1,15 @@
+package org.yunho.boardservice.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy,
+        String content
+) {
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
---
+++ src/main/java/org/yunho/boardservice/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package org.yunho.boardservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yunho.boardservice.dto.ArticleCommentDto;
+import org.yunho.boardservice.repository.ArticleCommentRepository;
+import org.yunho.boardservice.repository.ArticleRepository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
