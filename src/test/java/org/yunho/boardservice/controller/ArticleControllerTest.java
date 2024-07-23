ffc88928b42ab9995dee2b01952b9dc6364edd23
YunhoNam <nam100400@naver.com>
2024-07-23 12:55:43 +0900

    #48 - 중복 표현 삭제

--- src/test/java/org/yunho/boardservice/controller/ArticleControllerTest.java
+++ src/test/java/org/yunho/boardservice/controller/ArticleControllerTest.java
@@ -163,7 +163,6 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"))
-                .andExpect(model().attributeExists("articleComments"))
                 .andExpect(model().attribute("totalCount", totalCount));
         then(articleService).should().getArticleWithComments(articleId);
         then(articleService).should().getArticleCount();
