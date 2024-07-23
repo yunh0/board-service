5f3c347697a2bcef6f3ef1b1bf2071fee2494379
YunhoNam <nam100400@naver.com>
2024-07-23 18:21:17 +0900

    #63 - 대댓글 비즈니스 로직 구현

--- src/main/java/org/yunho/boardservice/service/ArticleCommentService.java
+++ src/main/java/org/yunho/boardservice/service/ArticleCommentService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.yunho.boardservice.domain.Article;
+import org.yunho.boardservice.domain.ArticleComment;
 import org.yunho.boardservice.domain.UserAccount;
 import org.yunho.boardservice.dto.ArticleCommentDto;
 import org.yunho.boardservice.repository.ArticleCommentRepository;
@@ -36,7 +37,14 @@ public class ArticleCommentService {
         try {
             Article article = articleRepository.getReferenceById(dto.articleId());
             UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
-            articleCommentRepository.save(dto.toEntity(article, userAccount));
+            ArticleComment articleComment = dto.toEntity(article, userAccount);
+
+            if (dto.parentCommentId() != null) {
+                ArticleComment parentComment = articleCommentRepository.getReferenceById(dto.parentCommentId());
+                parentComment.addChildComment(articleComment);
+            } else {
+                articleCommentRepository.save(articleComment);
+            }
         } catch (EntityNotFoundException e) {
             log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
         }
